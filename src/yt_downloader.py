c6f158acd8e74d2b772e2b687813329b1709c5ef
anshravalll <anshraval@gmail.com>
2024-09-28 23:51:37 +0530

    improved identation

--- src/yt_downloader.py
+++ src/yt_downloader.py
@@ -1,16 +1,15 @@
-import threading
-from yt_dlp import DownloadError, YoutubeDL
 import sys
 from pathlib import Path
 import threading
+from yt_dlp import DownloadError, YoutubeDL
 
+# Importing Telegram class from the yt_test module
 project_root = Path(__file__).resolve().parent.parent  # Go up two levels to project_root
 sys.path.insert(0, str(project_root))
-
 from tests.yt_test import Telegram
 
 
-class Downloader():
+class Downloader:
     def __init__(self, opts, id):
         url = self.default_url(id)
         if opts.get('cookiefile', None) or opts.get('cookiesfrombrowser', None):
@@ -19,17 +18,20 @@ class Downloader():
             self.plain_login(opts, url)
 
     def plain_login(self, opts, url):
-        opts['username'] = input("Type your email:")
-        opts['password'] = input("Type your password:")
+        """Method to handle plain login when no cookies are provided."""
+        opts['username'] = input("Type your email: ")
+        opts['password'] = input("Type your password: ")
         with YoutubeDL(opts) as ydl:
             ydl.download(url)
 
     def default_url(self, id):
+        """Returns the default URL for downloading membership videos."""
         default_url = 'https://www.youtube.com/playlist?list='
-        updated_url = default_url + 'UUMO' + id[2:] #Playlist for all membership videos for a channel
+        updated_url = default_url + 'UUMO' + id[2:]  # Playlist for all membership videos for a channel
         return updated_url
 
     def with_cookies(self, opts, url):
+        """Method to handle downloads using cookies."""
         with YoutubeDL(opts) as ydl:
             while True:
                 try:
@@ -37,74 +39,71 @@ class Downloader():
                 except DownloadError:
                     return
 
+
 def progress_hook(d):
+    """Hook to track download progress."""
     if d['status'] == 'finished':
         print(d['info_dict'].keys())
 
+
 if __name__ == "__main__":
     opts = {
-            'cookiefile': r"C:\Users\Ansh\Desktop\coding\yt-membership\tests\cookies.txt",
-        #'cookiesfrombrowser': ('firefox', 'C:/Users/Ansh/AppData/Local/Packages/Mozilla.Firefox_n80bbvh6b1yt2/LocalCache/Roaming/Mozilla/Firefox/Profiles/422oklgq.default-release'),
+        'cookiefile': r"C:\Users\Ansh\Desktop\coding\yt-membership\tests\cookies.txt",
+        # 'cookiesfrombrowser': ('firefox', 'C:/Users/Ansh/AppData/Local/Packages/Mozilla.Firefox_n80bbvh6b1yt2/LocalCache/Roaming/Mozilla/Firefox/Profiles/422oklgq.default-release'),
 
-            'outtmpl': {
-                'default': 'D:/Youtube/%(channel)s/%(playlist)s/Videos/%(title)s.%(ext)s',  # Video files
-                'thumbnail': 'D:/Youtube/%(channel)s/%(playlist)s/Thumbnails/%(title)s.%(ext)s',  # Thumbnails
-                'subtitle': 'D:/Youtube/%(channel)s/%(playlist)s/Subtitles/%(title)s.%(ext)s',  # Subtitles
-                'description': 'D:/Youtube/%(channel)s/%(playlist)s/Description/%(title)s.%(ext)s',  # Description
-                'infojson': 'D:/Youtube/%(channel)s/%(playlist)s/InfoJSON/%(title)s.%(ext)s',  # Info JSON
-},
+        'outtmpl': {
+            'default': 'D:/Youtube/%(channel)s/%(playlist)s/Videos/%(title)s.%(ext)s',  # Video files
+            'thumbnail': 'D:/Youtube/%(channel)s/%(playlist)s/Thumbnails/%(title)s.%(ext)s',  # Thumbnails
+            'subtitle': 'D:/Youtube/%(channel)s/%(playlist)s/Subtitles/%(title)s.%(ext)s',  # Subtitles
+            'description': 'D:/Youtube/%(channel)s/%(playlist)s/Description/%(title)s.%(ext)s',  # Description
+            'infojson': 'D:/Youtube/%(channel)s/%(playlist)s/InfoJSON/%(title)s.%(ext)s',  # Info JSON
+        },
 
-            'sleep_interval': 5,
-            'max_sleep_interval': 30,
-            'sleep_requests': 1,
+        'sleep_interval': 5,
+        'max_sleep_interval': 30,
+        'sleep_requests': 1,
 
-            # Error Handling
-            'ignoreerrors': True,
-            #'noprogress': True,
+        # Error Handling
+        'ignoreerrors': True,
+        # 'noprogress': True,
 
-            # File Handling
-            'nopart': False,  # Start fresh, do not continue downloads
-            'overwrite': False,  # Prevent overwriting of existing files
-            'download_archive': 'archive.log',  # Log downloaded videos to avoid re-downloading
+        # File Handling
+        'nopart': False,  # Start fresh, do not continue downloads
+        'overwrite': False,  # Prevent overwriting of existing files
+        'download_archive': 'archive.log',  # Log downloaded videos to avoid re-downloading
 
-            # Fragment Handling
-            'concurrent_fragment_downloads': 3,
+        # Fragment Handling
+        'concurrent_fragment_downloads': 3,
 
+        # Metadata Handling
+        'writedescription': True,  # Write video description to file
+        'writeinfojson': True,  # Write video info JSON to file
+        'writethumbnail': True,  # Write thumbnail image to file
+        'writesubtitles': True,  # Allow writing subtitles
+        'writeautomaticsub': True,
+        'allsubs': True,  # Download all subtitles
 
-            'writedescription': True,  # Write video description to file
-            'writeinfojson': True,  # Write video info JSON to file
-            'writethumbnail': True,  # Write thumbnail image to file
-            'writesubtitles': True,  # Allow writing subtitles
-            'writeautomaticsub': True,
-            'allsubs': True,  # Download all subtitles
+        # Progress-hook
+        'progress_hooks': [progress_hook]
+    }
 
-            #Progress-hook
-            'progress_hooks': [progress_hook]
-            
-            }
-
-    def outtmpl_to_path(output_tempelate): #Modifying metadata - yt-dlp documentation
+    def outtmpl_to_path(output_template):
+        """Placeholder function to modify metadata - per yt-dlp documentation."""
         pass
 
-            
+    # Channel ID for downloading membership videos
+    id = 'UCfQgsKhHjSyRLOp9mnffqVg'
 
-        
-
-
-    #outtmpl_to_path(opts['outtmpl'])
-
-    id = 'UCfQgsKhHjSyRLOp9mnffqVg' #channel id
-
-    #Downloader(opts, id)
+    # Initialize Telegram for upload functionality
     telegram_upload = Telegram().upload()
+
+    # Start downloading using Downloader class
     Downloader(opts, id)
 
-    # thread1 = threading.Thread(target = Downloader, args = (opts, id)) 
-    # thread2 = threading.Thread(target = telegram_upload.upload)
+    # Uncomment the below threading code if you want to run the Telegram upload in parallel
+    # thread1 = threading.Thread(target=Downloader, args=(opts, id))
+    # thread2 = threading.Thread(target=telegram_upload.upload)
     # thread1.start()
     # thread2.start()
     # thread1.join()
     # thread2.join()
-
-
-    
